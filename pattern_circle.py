1c219b4aa84ae741c880cbdd3a71a680c2060798
anshitasandoni-tech <anshitasandoni@gmail.com>
2026-07-31 16:03:48 +0530

    add new file

---
+++ pattern_circle.py
@@ -0,0 +1,14 @@
+import turtle
+t = turtle.Turtle()
+t.screen.bgcolor('black')
+t.speed(0)
+t.pensize(2)
+
+color = ['pink','hotpink','deeppink','palevioletred']
+for x in range (150):
+    t.color(color [x % 4])
+    t.circle(x) 
+    t.left(30)
+
+t.hideturtle()
+turtle.done()
